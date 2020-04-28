06c071b5c378bac94dbc7ce0c38fb0e74a147525
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-28 15:20:30 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment 
+  attr_accessor :name, :artist, :genre 
+  
+  @@all = []
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
+    @@all << self
+  end
+  
+  def Song.all
+    @@all
+  end
+end
